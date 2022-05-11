4a47468d22d7fd5aa25aeafd317eee6323542cfc
Poliana Guimarães <poli998881189@gmail.com>
2022-05-11 20:30:01 -0300

    first commit

---
+++ CalculaJuros/ModelRequest.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace CalculaJuros
+{
+    public class ModelRequest
+    {
+        public int valorinicial { get; set; }
+        public int meses { get; set; }
+
+        public int result  { get; set; }
+        public string url { get; set; }
+    }
+}
